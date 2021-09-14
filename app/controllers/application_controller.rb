c0fc8154d5fd1316915d1c963d35390e4e441411
Nancy Zaky <nancyzki@yahoo.com>
2021-09-13 17:42:40 -0700

    commit tree 41d120ea8b0cb57ffb2a45834eb2fb7e636493d8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
 
-  # add routes
+set :default_content_type, 'application/json'
+get '/bakeries' do
+Bakery.all.to_json
+end
+
+get '/bakeries/:id' do
+ bake= Bakery.find(params[:id])
+ bake.to_json(include: :baked_goods)
+end
+get  "/baked_goods/by_price" do
+  BakedGood.all.order("price DESC").to_json
+end
+get "/baked_goods/most_expensive" do
+result = BakedGood.all.order("price DESC")
+result[0].to_json
+
+end
+
 
 end
